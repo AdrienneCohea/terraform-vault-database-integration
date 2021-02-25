8489f736585a1601655a9e7075302e986baa2ea6
Adrienne Cohea <adriennecohea@gmail.com>
2021-02-24 22:22:24 -0800

    Cleanup: removed unused variable

--- postgresql/variables.tf
+++ postgresql/variables.tf
@@ -2,7 +2,3 @@ variable "backend" {}
 variable "postgresql_connection_url" {}
 variable "postgresql_roles" {}
 variable "database_name" {}
-variable "schema" {
-  type = string
-  default = "public"
-}
